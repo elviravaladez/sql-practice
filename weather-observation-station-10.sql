64e6275facfb17e686c195d3dd519fe7fbdf8e80
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-13 14:25:50 -0600

    Weather Observation Station 10
     Exercise

---
+++ weather-observation-station-10.sql
@@ -0,0 +1,13 @@
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (CITY NOT LIKE '%a'
+    AND CITY NOT LIKE '%e'
+    AND CITY NOT LIKE '%i'
+    AND CITY NOT LIKE '%o'
+    AND CITY NOT LIKE '%u');
+
+
+--SOLUTION 2
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '[^aiueo]$';
